6145922970e91d80b785d579f85911ad5ca6aefe
Janice.Burgos <Janice.Burgos@USTPALD1584>
2022-05-17 19:49:00 -0400

    Add project files.

---
+++ ATUniActivity8/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ATUniActivity8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ATUniActivity8")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86212d4f-944e-4f0f-a438-fbd5f6a369fd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ATUniActivity8/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace ATUniActivity8
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        WebDriver driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            driver.Navigate().GoToUrl("https://www.google.com");
+            string actualTitle = driver.Title;
+            Console.WriteLine(actualTitle);
+        }
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+        [TestCleanup]
+        public void Close()
+        {
+            driver.Quit();
+        }
+        }
+    }
+
